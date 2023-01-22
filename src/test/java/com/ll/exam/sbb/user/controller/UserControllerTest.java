9bd8ade7f11dd81512f03371bced5aa7e78e48be
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-22 21:15:21 +0900

    회원가입 테스트

--- src/test/java/com/ll/exam/sbb/user/controller/UserControllerTest.java
+++ src/test/java/com/ll/exam/sbb/user/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.ll.exam.sbb.user.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -21,7 +23,7 @@ class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Test
-    void signup() throws Exception {
+    void getSignup() throws Exception {
         ResultActions resultActions = mockMvc
                 .perform(get("/user/signup"))
                 .andDo(print());
@@ -29,6 +31,24 @@ class UserControllerTest {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(UserController.class))
                 .andExpect(handler().methodName("signup"))
-                .andExpect(content().string(containsString("회원가입")));
+                .andExpect(content().string(containsString("<h4>회원가입</h4>")));
+    }
+
+    @Test
+    void postSignup() throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(post("/user/signup")
+                        .with(csrf())
+                        .param("username", "user999")
+                        .param("password1", "1234")
+                        .param("password2", "1234")
+                        .param("email", "user999@test.com")
+                )
+                .andDo(print());
+        resultActions
+                .andExpect(status().is3xxRedirection())
+                .andExpect(handler().handlerType(UserController.class))
+                .andExpect(handler().methodName("signup"))
+                .andExpect(redirectedUrl("/"));
     }
 }
