6f93594d2e48a0b72c6d61d91f10aab1f332106b
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-24 10:48:38 +0900

    test 는 test DB에서 테스트 하도록 함

--- src/test/java/com/ll/exam/sbb/user/controller/UserControllerTest.java
+++ src/test/java/com/ll/exam/sbb/user/controller/UserControllerTest.java
@@ -1,14 +1,17 @@
 package com.ll.exam.sbb.user.controller;
 
-import org.junit.jupiter.api.DisplayName;
+import com.ll.exam.sbb.user.service.UserService;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -17,11 +20,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @Transactional
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private UserService userService;
     @Test
     void getSignup() throws Exception {
         ResultActions resultActions = mockMvc
@@ -50,5 +57,8 @@ class UserControllerTest {
                 .andExpect(handler().handlerType(UserController.class))
                 .andExpect(handler().methodName("signup"))
                 .andExpect(redirectedUrl("/"));
+        assertThat(userService.getUser("user999")).isNotNull();
+
     }
+
 }
