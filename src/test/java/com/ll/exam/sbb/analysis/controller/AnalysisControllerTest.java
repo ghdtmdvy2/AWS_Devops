65b0c5a08e4520f4ef6c735c2b74d0c0f0116515
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-24 16:21:36 +0900

    감정 목록 창 테스트 추가

---
+++ src/test/java/com/ll/exam/sbb/analysis/controller/AnalysisControllerTest.java
@@ -0,0 +1,37 @@
+package com.ll.exam.sbb.analysis.controller;
+
+import com.ll.exam.sbb.aritlce.controller.ArticleController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Transactional
+class AnalysisControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void get_article_list() throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(get("/analysis/list/1"))
+                .andDo(print());
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(AnalysisController.class))
+                .andExpect(handler().methodName("list"));
+    }
+}
