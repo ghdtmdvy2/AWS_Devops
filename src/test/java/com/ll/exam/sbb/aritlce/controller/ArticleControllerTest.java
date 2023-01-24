00ceba0f68b405862866722a7fd7c1ab336c690f
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-24 15:18:58 +0900

    게시판 수정페이지 get 요청 테스트 추가

--- src/test/java/com/ll/exam/sbb/aritlce/controller/ArticleControllerTest.java
+++ src/test/java/com/ll/exam/sbb/aritlce/controller/ArticleControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -44,5 +45,17 @@ class ArticleControllerTest {
                 .andExpect(status().is2xxSuccessful())
                 .andExpect(handler().handlerType(ArticleController.class))
                 .andExpect(handler().methodName("detail"));
+    }
+
+    @Test
+    @WithUserDetails("user1")
+    void get_modify_article() throws Exception {
+        ResultActions resultActions = mockMvc
+                .perform(get("/article/modify/1"))
+                .andDo(print());
+        resultActions
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(handler().handlerType(ArticleController.class))
+                .andExpect(handler().methodName("articleModify"));
     }
 }
