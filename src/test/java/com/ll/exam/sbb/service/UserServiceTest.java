09b221d193b47fb1f830ea19681f9dc8ad4e9f68
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-25 14:40:57 +0900

    유저 비밀번호 수정 테스트 추가

--- src/test/java/com/ll/exam/sbb/service/UserServiceTest.java
+++ src/test/java/com/ll/exam/sbb/service/UserServiceTest.java
@@ -20,7 +20,7 @@ class UserServiceTest {
     @Autowired
     UserService userService;
     @Test
-    public void userSignup(){
+    public void user_signup(){
         String username = "user3";
         String email = "user3@test.com";
         String password = "1234";
@@ -29,6 +29,23 @@ class UserServiceTest {
         SiteUser user = userService.getUser(username);
         assertThat(user.getUsername()).isNotNull();
         assertThat(user.getEmail()).isNotNull();
-        assertThat(passwordEncoder.matches(password, user.getPassword())).isNotNull();
+        assertThat(passwordEncoder.matches(password, user.getPassword())).isTrue();
+    }
+
+    @Test
+    public void user_information_update(){
+
+        String oldPassword = "1234";
+        String changePassword = "4321";
+        SiteUser user = userService.getUser("user1");
+
+        assertThat(passwordEncoder.matches(oldPassword,user.getPassword()));
+
+        userService.update(user,changePassword);
+
+        assertThat(user.getUsername()).isNotNull();
+        assertThat(user.getEmail()).isNotNull();
+        assertThat(passwordEncoder.matches(oldPassword, user.getPassword())).isFalse();
+        assertThat(passwordEncoder.matches(changePassword, user.getPassword())).isTrue();
     }
 }
