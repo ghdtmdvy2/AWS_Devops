58ecedcea9d5358fb786510c881c0cf2b8c37dba
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-25 15:44:57 +0900

    게시판 리스트 조회 테스트 추가

---
+++ src/test/java/com/ll/exam/sbb/service/ArticleServiceTest.java
@@ -0,0 +1,29 @@
+package com.ll.exam.sbb.service;
+
+import com.ll.exam.sbb.aritlce.entity.Article;
+import com.ll.exam.sbb.aritlce.service.ArticleService;
+import com.ll.exam.sbb.user.entity.SiteUser;
+import com.ll.exam.sbb.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+class ArticleServiceTest {
+    @Autowired
+    ArticleService articleService;
+    @Autowired
+    UserService userService;
+    @Test
+    public void get_article_list(){
+        Page<Article> articles = articleService.getList("", 0, "");
+        assertThat(articles.getSize()).isGreaterThan(0);
+    }
+}
