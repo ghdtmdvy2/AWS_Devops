af74f63d6451ebe8a951409a70345c58c868712f
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-01-27 16:23:58 +0900

    상품 Entity 추가

---
+++ src/main/java/com/ll/exam/sbb/product/entity/Product.java
@@ -0,0 +1,26 @@
+package com.ll.exam.sbb.product.entity;
+
+import com.ll.exam.sbb.base.config.BaseTimeEntity;
+import com.ll.exam.sbb.user.entity.SiteUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+public class Product extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String subject;
+    private String content;
+    private int price;
+    @ManyToOne
+    private SiteUser author;
+}
