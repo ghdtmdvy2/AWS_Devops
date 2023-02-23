94be58dfdff25aea1b08e1894284ba92e7e1e5f0
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2023-02-23 16:22:16 +0900

    주문 상세 페이지 html 수정

--- src/main/java/com/example/driveanalysis/order/repository/ProductOrderRepository.java
+++ src/main/java/com/example/driveanalysis/order/repository/ProductOrderRepository.java
@@ -8,10 +8,14 @@ import org.springframework.data.repository.query.Param;
 
 import javax.persistence.Entity;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductOrderRepository extends JpaRepository<ProductOrder,Long> {
 
     @EntityGraph(attributePaths = "orderItems")
     @Query(value = "select po from ProductOrder po join fetch po.orderer where po.orderer.id = :userId and po.isPaid = True")
     List<ProductOrder> findByIsPaidTrueAndOrdererId(@Param("userId") Long userId);
+    @Override
+    @Query(value = "select po from ProductOrder po join fetch po.orderItems poo join fetch poo.product where po.id = :productOrderId")
+    Optional<ProductOrder> findById(@Param("productOrderId") Long productOrderId);
 }
